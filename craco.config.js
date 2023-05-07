5228d907246aaee8153407915379532a29c07ef6
jasonshu1229 <shuhao1229@163.com>
2023-05-07 15:23:27 +0800

    feat: 项目环境搭建

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
+const cracoLess = require('craco-less')
+
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
+  plugins: [{ plugin: cracoLess }],
   webpack: {
     alias: {
       '@': resolve('src'),
